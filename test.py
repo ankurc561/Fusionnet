b9b74c58e0d2073fad2cfca252ea033cd9bc6cdc
Ankur Chauhan <ankurc561@gmail.com>
2026-05-28 01:39:03 +0530

    git test

--- test.py
+++ test.py
@@ -3,3 +3,8 @@ print("Hello World")
 def add(a: int, b: int) -> int:
     return a + b
 
+def multiply(a: int, b:int) -> int:
+    return a * b
+
+def divide(a: int, b: int) -> float:
+    return a / b
